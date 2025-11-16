4f80a8a70caef4f517c1739bc6cef860dd208ccb
Coorchak <153365391+Coorchak@users.noreply.github.com>
2025-11-16 12:45:50 +0100

    Enhance comments with Terraform best practices
    Added comments on best practices for Terraform usage.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
 # 1. Dość stara wersja providera, juz dawno jest 7.x, ale pod kątem samego działania nic to dla Ciebie nie zmienia. W pracy po prostu pilnuje się, aby odpalając nowe projekty korzystać z możliwie najnowszej wersji
 # 2. Pewnie w tym projekcie nie ma to takiego znaczenia, ale tak szczerze to rzadko kiedy się pisze Terraformy takie stand alone. Standardem jest tworzenie reużywalnych modułów (https://developer.hashicorp.com/terraform/language/modules). Moduły to taka powtarzalna logika, którą można porównać do bibliotek w programowaniu.
+# 3. Kolejna rzecz z dobrych praktyk to podawanie minimalnej wersji Terraforma do powołania środowiska (https://developer.hashicorp.com/terraform/language/block/terraform#required_version). Pozwala to uniknąć sytuacji takich, że kod korzysta z funkcji, których na przykład nie ma w wersji tf na której go odpalasz.
+
 terraform {
   required_providers {
     google = { source = "hashicorp/google", version = "~> 6.0" }
