1924a98919c221f57ae9a56c946c49869beae6f5
Coorchak <153365391+Coorchak@users.noreply.github.com>
2025-11-16 13:01:01 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,5 @@
+# 1. w outputach podobnie jak przy variables dobrą praktyką jest dawanie opisu
+
 output "pubsub_topic_jobs_raw" {
   value = google_pubsub_topic.jobs_raw.id
 }
@@ -7,5 +9,5 @@ output "extract_function_uri" {
 }
 
 output "jobs_raw_table" {
-  value = "${var.project_id}.${google_bigquery_dataset.laborinsight.dataset_id}.${google_bigquery_table.jobs_raw.table_id}"
+  value = "${var.project_id}.${google_bigquery_dataset.laborinsight.dataset_id}.${google_bigquery_table.jobs_raw.table_id}" <--------------- brzydki sposób interpolacji, tak optycznie mi się nie podoba i wygląda jak za starego Terraforma. Z ciekawości zapytałem AI, jakby to zapisał i podpowiedział coś takiego 'value       = format("%s.%s.%s", var.project_id, google_bigquery_dataset.laborinsight.dataset_id, google_bigquery_table.jobs_raw.table_id)', ale nie mam 100% pewności działania, bo nie testowałem :)
 }
