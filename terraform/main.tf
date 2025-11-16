4a850355877a8a709fe08a24c09ab2262075e30e
Coorchak <153365391+Coorchak@users.noreply.github.com>
2025-11-16 12:25:49 +0100

    Add comments on provider version and module best practices
    Added comments regarding provider version and module usage.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+# 1. Dość stara wersja providera, juz dawno jest 7.x, ale pod kątem samego działania nic to dla Ciebie nie zmienia. W pracy po prostu pilnuje się, aby odpalając nowe projekty korzystać z możliwie najnowszej wersji
+# 2. Pewnie w tym projekcie nie ma to takiego znaczenia, ale tak szczerze to rzadko kiedy się pisze Terraformy takie stand alone. Standardem jest tworzenie reużywalnych modułów (https://developer.hashicorp.com/terraform/language/modules). Moduły to taka powtarzalna logika, którą można porównać do bibliotek w programowaniu.
 terraform {
   required_providers {
     google = { source = "hashicorp/google", version = "~> 6.0" }
