73b66f102199328e6f37822c4939ee9ea163e758
Karol Kurek <kurekhombre@gmail.com>
2025-11-30 16:45:34 +0100

    del data_persistent_disk_spec - quota issue

--- terraform/colab_runtime_template.tf
+++ terraform/colab_runtime_template.tf
@@ -6,10 +6,12 @@ resource "google_colab_runtime_template" "protocol_runtime" {
     machine_type = "e2-standard-4"
   }
 
-  data_persistent_disk_spec {
-    disk_type    = "pd-standard" 
-    disk_size_gb = 20 # sprawdzic to. byc moze przy kazdej inicjacji bedzie tworzyc nowy dysk. wysiwietlal sie w przezszlosci blad error code 8 "Quota'SSD_TOTAL_GB' exceeded". Byc moze problem jest natury eu-central. doczytac czym jest ta quota
-  }
+  # data_persistent_disk_spec {
+  #   disk_type    = "pd-standard" 
+  #   disk_size_gb = 20 
+  #   # sprawdzic to. byc moze przy kazdej inicjacji bedzie tworzyc nowy dysk. wysiwietlal sie w przezszlosci blad error code 8 "Quota'SSD_TOTAL_GB' exceeded". 
+  #   # Byc moze problem jest natury eu-central. doczytac czym jest ta quota (nie wiem)
+  # }
 
   network_spec {
     enable_internet_access = true
